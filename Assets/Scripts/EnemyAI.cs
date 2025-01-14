781e3a154193d5c202c85a6b24a28f8c5224fcfa
Eileitheiyax <toprakgoktug99@gmail.com>
2025-01-14 15:13:31 +0300

    Animasyonlara event eklendi

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,9 +5,11 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    [SerializeField] Transform target;
-    [SerializeField] float chasingRadius = 6f;
-    [SerializeField] RuntimeAnimatorController animatorController; // Her prefab için farklý Animator Controller
+    [SerializeField] Transform target;             // Hedef oyuncu
+    [SerializeField] float chasingRadius = 6f;     // Takip yarýçapý
+    [SerializeField] GameObject attackEffectPrefab; // Saldýrý sýrasýnda oynatýlacak efekt prefabý
+    [SerializeField] Transform effectSpawnPoint;   // Efektin çýkýþ noktasý
+    [SerializeField] int attackDamage = 10;        // Düþmanýn verdiði hasar miktarý
 
     private NavMeshAgent navMeshAgent;
     private Animator animator;
@@ -18,12 +20,6 @@ public class EnemyAI : MonoBehaviour
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
-
-        // Prefab'a özgü Animator Controller'ý ata
-        if (animatorController != null)
-        {
-            animator.runtimeAnimatorController = animatorController;
-        }
     }
 
     void Update()
@@ -64,6 +60,31 @@ public class EnemyAI : MonoBehaviour
         animator.SetBool("attack", false);
         animator.SetTrigger("move");
         navMeshAgent.SetDestination(target.position);
+    }
+
+    // Animasyon olayý ile çaðrýlacak fonksiyon
+    public void PerformAttack()
+    {
+        // Hasar verme iþlemi
+        DealDamageToPlayer();
+
+        // Efekt oynatma
+        if (attackEffectPrefab != null && effectSpawnPoint != null)
+        {
+            Instantiate(attackEffectPrefab, effectSpawnPoint.position, effectSpawnPoint.rotation);
+        }
+
+        Debug.Log("Enemy performed an attack!");
+    }
+
+    private void DealDamageToPlayer()
+    {
+        // Oyuncuya hasar verme iþlemi
+        PlayerHealth playerHealth = FindObjectOfType<PlayerHealth>();
+        if (playerHealth != null)
+        {
+            playerHealth.TakeDamage(attackDamage);
+        }
     }
 
     void OnDrawGizmosSelected()
