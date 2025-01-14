0915b5f8cff723b9223b7de6a74826e3ecc818dd
Eileitheiyax <toprakgoktug99@gmail.com>
2025-01-14 14:41:51 +0300

    Animasyon var

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,33 +7,39 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] float chasingRadius = 6f;
+    [SerializeField] RuntimeAnimatorController animatorController; // Her prefab için farklý Animator Controller
 
-    NavMeshAgent navMeshAgent;
-    float targetDistance = Mathf.Infinity;
-    bool isProved = false;
+    private NavMeshAgent navMeshAgent;
+    private Animator animator;
+    private float targetDistance = Mathf.Infinity;
+    private bool isProved = false;
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        // Prefab'a özgü Animator Controller'ý ata
+        if (animatorController != null)
+        {
+            animator.runtimeAnimatorController = animatorController;
+        }
     }
 
-    
     void Update()
     {
         targetDistance = Vector3.Distance(target.position, transform.position);
 
-        if(isProved )
+        if (isProved)
         {
             DelayWithTarget();
         }
-
         else if (targetDistance <= chasingRadius)
         {
             isProved = true;
         }
-
     }
-     
+
     private void DelayWithTarget()
     {
         if (targetDistance >= navMeshAgent.stoppingDistance)
@@ -50,22 +55,20 @@ public class EnemyAI : MonoBehaviour
 
     private void AttackTarget()
     {
-        GetComponent<Animator>().SetBool("attack", true);
-        Debug.Log(name + "is destroying" + target.name);
+        animator.SetBool("attack", true);
+        Debug.Log(name + " is attacking " + target.name);
     }
 
     private void ChaseTarget()
     {
-        GetComponent<Animator>().SetBool("attack",false);
-        GetComponent<Animator>().SetTrigger("move");
+        animator.SetBool("attack", false);
+        animator.SetTrigger("move");
         navMeshAgent.SetDestination(target.position);
     }
 
-     void OnDrawGizmosSelected()
+    void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, chasingRadius);
     }
-
 }
-
